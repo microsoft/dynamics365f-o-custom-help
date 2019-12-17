67e21e4b1874c6f535d0f082ad04681593109401
John Wilcock <jowilco@microsoft.com>
2019-12-17 12:12:48 -0800

    Updated ConvertHtmlToJson to only copy specific metadata values

--- SourceCode/ConvertHtmlToJson/ConvertHtmlToJson/HTMLtoJSONProcessor.cs
+++ SourceCode/ConvertHtmlToJson/ConvertHtmlToJson/HTMLtoJSONProcessor.cs
@@ -13,6 +13,7 @@ namespace ConvertHtmlToJson
         private readonly HTMLDirectory htmlDirectory;
         private readonly JSONDirectory jsonDirectory;
         private readonly bool verbose;
+        private string firstLocale = string.Empty;
 
         public HTMLtoJSONProcessor(HTMLDirectory htmlDirectory, JSONDirectory jsonDirectory, bool verbose)
         {
@@ -21,54 +22,110 @@ namespace ConvertHtmlToJson
             this.verbose = verbose;
         }
 
-        public void Process()
+        public int Process()
         {
-            ProcessFilesInDirectory(htmlDirectory.GetDirectoryInfo());
+            return ProcessFilesInDirectory(htmlDirectory.GetDirectoryInfo());
         }
 
-        private void ProcessFilesInDirectory(DirectoryInfo dirInfo)
+        private int ProcessFilesInDirectory(DirectoryInfo dirInfo)
         {
+            int errorCount = 0;
             FileInfo[] dirFiles = dirInfo.GetFiles("*.html", SearchOption.TopDirectoryOnly);
             if (dirFiles.Length > 0)
             {
                 DirectoryInfo targetDirectory = CreateTargetDirectory(dirInfo);
                 foreach (FileInfo file in dirFiles)
                 {
-                    string jsonFilePath = Path.Combine(targetDirectory.FullName, Path.GetFileNameWithoutExtension(file.Name) + ".json");
-                    if (this.verbose)
-                    {
-                        Console.WriteLine("Processing " + file.FullName + " to " + jsonFilePath);
-                    }
                     if (file.FullName.IndexOf("toc.html", StringComparison.OrdinalIgnoreCase) == -1)
                     {
-                        int i = 0;
+                        string jsonFilePath = Path.Combine(targetDirectory.FullName, Path.GetFileNameWithoutExtension(file.Name) + ".json");
+                        if (this.verbose)
+                        {
+                            Console.WriteLine("Processing " + file.FullName + " to " + jsonFilePath);
+                        }
                         HtmlDocument htmlDoc = new HtmlDocument();
                         htmlDoc.Load(file.FullName);
 
                         JObject product = new JObject();
                         HtmlNodeCollection nodeMeta = htmlDoc.DocumentNode.SelectNodes("//meta");
 
-                        string tag = string.Empty;
-                        string value = string.Empty;
+                        bool hasTitle = false;
+                        bool hasDescription = false;
+                        bool hasMSLocale = false;
                         foreach (HtmlNode _htm in nodeMeta)
                         {
-                            i++;
-                            if (i > 2)
+                            string tag = string.Empty;
+                            string value = string.Empty;
+                            HtmlAttributeCollection attribColl = _htm.Attributes;
+                            if (attribColl[0].Value == "title")
                             {
-                                HtmlAttributeCollection attribColl = _htm.Attributes;
-                                if (attribColl[0].Value == "title" || attribColl[0].Value == "description")
+                                tag = attribColl[0].Value;
+                                value = WebUtility.HtmlDecode(attribColl[1].Value.ToString());
+                                product.Add(new JProperty(tag, value));
+                                if (!String.IsNullOrWhiteSpace(value))
                                 {
-                                    tag = attribColl[0].Value;
-                                    value = WebUtility.HtmlDecode(attribColl[1].Value.ToString());
+                                    hasTitle = true;
                                 }
-                                else
+                            }
+                            if (attribColl[0].Value == "description")
+                            {
+                                tag = attribColl[0].Value;
+                                value = WebUtility.HtmlDecode(attribColl[1].Value.ToString());
+                                product.Add(new JProperty(tag, value));
+                                if (!String.IsNullOrWhiteSpace(value))
                                 {
-                                    tag = attribColl[0].Value;
-                                    value = attribColl[1].Value.ToString();
+                                    hasDescription = true;
                                 }
+                            }
+                            if (attribColl[0].Value == "ms.locale")
+                            {
+                                if (String.IsNullOrWhiteSpace(firstLocale))
+                                {
+                                    if (String.IsNullOrWhiteSpace(attribColl[1].Value))
+                                    {
+                                        Console.WriteLine("ms.locale requires a value in " + file.FullName);
+                                    }
+                                    else
+                                    {
+                                        firstLocale = attribColl[1].Value;
+                                    }
+                                }
+                                tag = attribColl[0].Value;
+                                value = attribColl[1].Value.ToString();
+                                if (value.CompareTo(firstLocale) != 0)
+                                {
+                                    Console.WriteLine("ms.locale metadata value (" + value + ") in " + file.FullName + " does not match the first ms.locale metadata value found (" + firstLocale + "). All ms.locale metadata values should be identical.");
+                                    errorCount++;
+                                }
+                                product.Add(new JProperty(tag, value));
+                                if (!String.IsNullOrWhiteSpace(value))
+                                {
+                                    hasMSLocale = true;
+                                }
+                            }
+                            if (attribColl[0].Value == "ms.search.form" || attribColl[0].Value == "ms.search.scope" || attribColl[0].Value == "ms.search.region")
+                            {
+                                tag = attribColl[0].Value;
+                                value = attribColl[1].Value.ToString();
                                 product.Add(new JProperty(tag, value));
                             }
                         }
+                        if (!hasTitle)
+                        {
+                            Console.WriteLine("title metadata missing in " + file.FullName);
+                            errorCount++;
+                        }
+                        if (!hasDescription)
+                        {
+                            Console.WriteLine("description metadata missing in " + file.FullName);
+                            errorCount++;
+                        }
+                        if (!hasMSLocale)
+                        {
+                            Console.WriteLine("ms.locale metadata missing in " + file.FullName);
+                            errorCount++;
+                        }
+
 
                         HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//body");
                         product.Add(new JProperty("Content", node.InnerText));
@@ -83,8 +140,9 @@ namespace ConvertHtmlToJson
             }
             foreach (DirectoryInfo subdirectoryInfo in dirInfo.GetDirectories())
             {
-                ProcessFilesInDirectory(subdirectoryInfo);
+                errorCount = errorCount + (ProcessFilesInDirectory(subdirectoryInfo));
             }
+            return errorCount;
         }
 
         private DirectoryInfo CreateTargetDirectory(DirectoryInfo dirInfo)
--- SourceCode/ConvertHtmlToJson/ConvertHtmlToJson/Program.cs
+++ SourceCode/ConvertHtmlToJson/ConvertHtmlToJson/Program.cs
@@ -17,9 +17,6 @@ namespace ConvertHtmlToJson
         [Option('j', "jsonDirectory", Required = true, HelpText = "Directory where the JSON files should be created.")]
         public string JsonDirectory { get; set; }
 
-        [Option('l', "locale", Required = true, HelpText = "Locale of the HTML source files. JSON files must contain the locale in order for search to support the locale.")]
-        public string Locale { get; set; }
-
         [Option('v', "verbose", Required = false, HelpText = "Detailed logging for each file processed.")]
         public bool Verbose { get; set; }
     }
@@ -35,19 +32,14 @@ namespace ConvertHtmlToJson
         {
             try
             {
-                //HelpLocale helpLocale = new HelpLocale(opts.locale);
-                //CultureInfo cultureInfo = helpLocale.GetCultureInfo();
-                //Console.WriteLine(cultureInfo.DisplayName);
-
                 JSONDirectory jsonDirectory = new JSONDirectory(opts.JsonDirectory);
                 HTMLDirectory htmlDirectory = new HTMLDirectory(opts.HtmlDirectory);
 
                 HTMLtoJSONProcessor processor = new HTMLtoJSONProcessor(htmlDirectory, jsonDirectory, opts.Verbose);
-                processor.Process();
-            }
-            catch (CultureNotFoundException)
-            {
-                Console.WriteLine(opts.Locale + " is not a supported locale.");
+                if (processor.Process() > 0)
+                {
+                    Console.WriteLine("Please fix all of the errors listed and rerun ConvertHtmlToJson.");
+                }
             }
             catch (Exception ex)
             {
